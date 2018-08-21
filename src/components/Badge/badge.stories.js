06f7679fcbd6d63ac7b459dbccc35d0536e4d1ba
Joachim Dalen <me@d3v.no>
2018-08-21 19:21:19 +0200

    Add more badge variants to Storybook

--- src/components/Badge/badge.stories.js
+++ src/components/Badge/badge.stories.js
@@ -11,4 +11,19 @@ storiesOf("Badge", module)
       </BadgeAddon>
       npm install -s @dalenapps/dizzy
     </Badge>
+  ))
+  .add("Colors", () => (
+    <div>
+      <Badge className={`dz-badge-dark-blue`}>Dark blue</Badge>
+      <Badge className={`dz-badge-light-blue`}>Light Blue</Badge>
+      <Badge className={`dz-badge-muted`}>Muted</Badge>
+      <Badge className={`dz-badge-blue`}>Blue</Badge>
+      <Badge className={`dz-badge-primary`}>Primary</Badge>
+      <Badge className={`dz-badge-dark`}>Dark</Badge>
+      <Badge className={`dz-badge-success`}>Success</Badge>
+      <Badge className={`dz-badge-danger`}>Danger</Badge>
+      <Badge className={`dz-badge-warning`}>Warning</Badge>
+      <Badge className={`dz-badge-purple`}>Purple</Badge>
+      <Badge className={`dz-badge-white`}>White</Badge>
+    </div>
   ));
