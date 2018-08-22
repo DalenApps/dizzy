2afa08828124d3a4aa35c711e59c578a6d3f68cd
Joachim Dalen <me@d3v.no>
2018-08-22 20:30:14 +0200

    Refactor TextArea to use scss

--- src/components/TextArea/TextArea.js
+++ src/components/TextArea/TextArea.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
-import './TextArea.css';
+import './text-area.scss';
 
 class TextArea extends Component {
   static propTypes = {
