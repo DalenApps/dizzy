309ee998744d0661825a02941ee70ae96e5fe5b6
Joachim Dalen <me@d3v.no>
2018-08-22 18:59:34 +0200

    Refactor Divider to use scss

--- src/components/Divider/Divider.js
+++ src/components/Divider/Divider.js
@@ -1,20 +1,23 @@
 import React, { Component } from 'react';
+import cx from 'classnames';
 import PropTypes from 'prop-types';
-import './Divider.css';
+import './divider.scss';
 class Divider extends Component {
   static propTypes = {
+    className: PropTypes.string,
     centered: PropTypes.bool,
     title: PropTypes.string
   };
   static defaultProps = {
+    className: '',
     centered: false,
     title: 'Default'
   };
 
   render() {
-    const { centered, title } = this.props;
+    const { centered, title, className } = this.props;
     return (
-      <div className="dz-divider">
+      <div className={cx('dz-divider', className)}>
         {centered ? <span className="dz-divider-line" /> : ''}
         <span className="dz-divider-title">{title}</span>
         <span className="dz-divider-line" />
