9b1a4b894dddf5727f0abff542ea5387eeba7a26
Joachim Dalen <me@d3v.no>
2018-08-24 21:02:43 +0200

    Add condensed accordion

--- src/components/Accordion/accordion.stories.js
+++ src/components/Accordion/accordion.stories.js
@@ -39,4 +39,14 @@ storiesOf('Accordion', module)
         </p>
       </Accordion>
     </div>
+  ))
+  .add('Condensed', () => (
+    <Accordion className="dz-accordion-condensed">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+    </Accordion>
   ));
