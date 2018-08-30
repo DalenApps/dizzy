bfd13f2c51f6e372eac47d3d554baac04081d17c
Devexer <me@d3v.no>
2018-08-30 07:46:57 +0200

    Refactor Button to move classes into props

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -5,27 +5,73 @@ import PropTypes from 'prop-types';
 class Button extends Component {
   static propTypes = {
     className: PropTypes.string,
-    href: PropTypes.string,
     children: PropTypes.oneOfType([
       PropTypes.arrayOf(PropTypes.node),
       PropTypes.node
-    ]).isRequired
+    ]).isRequired,
+    size: PropTypes.oneOf(['small', 'normal', 'large']),
+    variant: PropTypes.oneOf([
+      'primary',
+      'darkblue',
+      'lightblue',
+      'muted',
+      'blue',
+      'dark',
+      'success',
+      'danger',
+      'warning',
+      'purple',
+      'white'
+    ]),
+    href: PropTypes.string,
+    outlined: PropTypes.bool,
+    noStyle: PropTypes.bool,
+    rounded: PropTypes.bool,
+    iconOnly: PropTypes.bool
   };
   static defaultProps = {
-    className: 'dz-button-primary',
-    href: null
+    className: '',
+    size: 'normal',
+    variant: 'primary',
+    href: null,
+    outlined: false,
+    noStyle: false,
+    rounded: false,
+    iconOnly: false
   };
 
   render() {
-    const { href, className, children } = this.props;
+    const {
+      href,
+      className,
+      children,
+      variant,
+      outlined,
+      noStyle,
+      rounded,
+      size,
+      iconOnly
+    } = this.props;
+    const styleClasses = cx(
+      { [`dz-button-${variant}`]: !outlined && !noStyle },
+      { [`dz-button-outline-${variant}`]: outlined && !noStyle },
+      { 'dz-button-link': noStyle },
+      { 'dz-button-rounded': rounded },
+      { [`dz-button-${size}`]: size !== 'normal' },
+      { 'dz-icon-only': iconOnly }
+    );
     if (href) {
       return (
-        <a href={href} className={cx('dz-button', className)}>
+        <a href={href} className={cx('dz-button', styleClasses, className)}>
           {children}
         </a>
       );
     }
-    return <button className={cx('dz-button', className)}>{children}</button>;
+    return (
+      <button className={cx('dz-button', styleClasses, className)}>
+        {children}
+      </button>
+    );
   }
 }
 
