5594873f916dff0af3a520c87584b6b98e69e933
Devexer <me@d3v.no>
2018-08-30 10:48:59 +0200

    Add loading state to button

--- src/components/Button/button.stories.js
+++ src/components/Button/button.stories.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { boolean } from '@storybook/addon-knobs';
 import { Button } from '../../';
 const btnTypes = [
   'primary',
@@ -86,4 +87,9 @@ storiesOf('Button', module)
         Large
       </Button>
     </div>
+  ))
+  .add('States', () => (
+    <Button onClick={action('clicked')} loading={boolean('Loading', false)}>
+      Not Loading
+    </Button>
   ));
