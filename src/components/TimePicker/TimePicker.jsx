ffb92e74e44a630a3f87acc939e9689b9c5beed5
Joachim Dalen <me@d3v.no>
2018-09-07 20:24:28 +0200

    Add moment and more props

--- src/components/TimePicker/TimePicker.jsx
+++ src/components/TimePicker/TimePicker.jsx
@@ -5,11 +5,30 @@ import { Badge, RadioButton } from '../..';
 class TimePicker extends Component {
   static propTypes = {
     className: PropTypes.string,
-    closeOnFocusLost: PropTypes.bool
+    closeOnFocusLost: PropTypes.bool,
+    footerComponent: PropTypes.node,
+    defaultValue: PropTypes.string,
+    disabled: PropTypes.bool,
+    format: PropTypes.string,
+    defaultHourStep: PropTypes.number,
+    defaultMinuteStep: PropTypes.number,
+    defaultSecondStep: PropTypes.number,
+    use24Hour: PropTypes.bool,
+    value: PropTypes.string,
+    onChange: PropTypes.func
   };
   static defaultProps = {
     className: '',
-    closeOnFocusLost: true
+    closeOnFocusLost: true,
+    footerComponent: null,
+    disabled: false,
+    format: 'HH:mm:ss',
+    defaultHourStep: 1,
+    defaultMinuteStep: 1,
+    defaultSecondStep: 1,
+    use24Hour: false,
+    value: null,
+    onChange: () => {}
   };
   constructor(props) {
     super(props);
@@ -21,10 +40,33 @@ class TimePicker extends Component {
       selectedTOD: 'AM'
     };
   }
+  pad(n, width, z) {
+    z = z || '0';
+    n = n + '';
+    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+  }
+  renderSec() {
+    const { defaultSecondStep } = this.props;
+    let items = [];
+    for (let i = 0; i < 60; i = i + defaultSecondStep) {
+      items.push(<li>{this.pad(i, 2)}</li>);
+    }
+    return items;
+  }
+
+  renderHrs() {
+    const { defaultHourStep } = this.props;
+    let items = [];
+    for (let i = 0; i < 60; i = i + defaultHourStep) {
+      items.push(<li>{i}</li>);
+    }
+    return items;
+  }
 
   renderMin() {
+    const { defaultMinuteStep } = this.props;
     let items = [];
-    for (let i = 10; i < 60; i++) {
+    for (let i = 0; i < 60; i = i + defaultMinuteStep) {
       items.push(<li>{i}</li>);
     }
     return items;
@@ -51,7 +93,7 @@ class TimePicker extends Component {
             <div className={cx('dz-timepicker-column')}>
               <ul>
                 <li className={cx('dz-timepicker-list-header')}>Hrs</li>
-                {this.renderMin()}
+                {this.renderHrs()}
               </ul>
             </div>
             <div className={cx('dz-timepicker-column')}>
@@ -63,7 +105,7 @@ class TimePicker extends Component {
             <div className={cx('dz-timepicker-column')}>
               <ul>
                 <li className={cx('dz-timepicker-list-header')}>Sec</li>
-                {this.renderMin()}
+                {this.renderSec()}
               </ul>
             </div>
           </div>
