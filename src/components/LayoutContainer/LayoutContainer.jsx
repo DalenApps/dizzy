40b545174dc353f261f5c7e0d44254506221a6fa
Joachim Dalen <me@d3v.no>
2018-11-03 19:58:09 +0100

    Add LayoutContainer V1

--- src/components/LayoutContainer/LayoutContainer.jsx
+++ src/components/LayoutContainer/LayoutContainer.jsx
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import cx from 'classnames';
 import PropTypes from 'prop-types';
+import { childProps } from '../../helpers';
 class LayoutContainer extends Component {
   static propTypes = {
-    className: PropTypes.string
+    className: PropTypes.string,
+    narrow: PropTypes.bool,
+    children: childProps
   };
   static defaultProps = {
-    className: ''
+    className: '',
+    narrow: false
   };
 
   render() {
-    const { className } = this.props;
-    return <div className={cx('dz-layoutcontainer', className)}>d</div>;
+    const { className, children, narrow } = this.props;
+    const containerClass = cx(
+      { 'dz-container': !narrow },
+      { 'dz-container-fluid': narrow },
+      className
+    );
+    return <div className={containerClass}>{children}</div>;
   }
 }
 
--- src/components/LayoutContainer/layoutcontainer.stories.js
+++ src/components/LayoutContainer/layoutcontainer.stories.js
@@ -1,4 +1,13 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { LayoutContainer } from '../../';
-storiesOf('LayoutContainer', module).add('Default', () => <LayoutContainer />);
+import { LayoutContainer, Grid, GridItem } from '../../';
+import { boolean } from '@storybook/addon-knobs';
+storiesOf('LayoutContainer', module).add('Default', () => (
+  <LayoutContainer narrow={boolean('Narrow/Fluid', false)}>
+    <Grid>
+      <GridItem xlarge={4}>Hello</GridItem>
+      <GridItem xlarge={8}>Hello</GridItem>
+      <GridItem xlarge={12}>Hello</GridItem>
+    </Grid>
+  </LayoutContainer>
+));
