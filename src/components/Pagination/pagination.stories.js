9114ce2d875d68c92bf3ae46c2d253f3dcb3413f
Joachim Dalen <me@d3v.no>
2018-11-03 21:54:07 +0100

    Refactor pagination component

--- src/components/Pagination/pagination.stories.js
+++ src/components/Pagination/pagination.stories.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Pagination } from '../../';
-storiesOf('Paginator', module).add('Default', () => <Pagination />);
+import { number, boolean } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+storiesOf('Paginator', module).add('Default', () => (
+  <Pagination
+    currentPage={number('currentPage', 10)}
+    totalPages={number('totalPages', 110)}
+    zeroIndexStart={boolean('zeroIndexStart', false)}
+    onPageChange={action('onPageChange')}
+    buttonVariant="success"
+  />
+));
